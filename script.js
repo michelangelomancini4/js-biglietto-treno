fcdfaeacf2991ef343a30c15c75352c43b16e8a6
michelangelomancini <michelangelomancini8@gmail.com>
2024-12-17 15:46:28 +0100

    fixing diciture condizioni

--- script.js
+++ script.js
@@ -4,9 +4,6 @@
 // - va applicato uno sconto del 40% per gli over 65.
 // OUTPUT- L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
-
-
-
 // INPUT- come input chiediamo all'utente (passeggero) di inserire il numero di chilometri che vuole percorrere e l'età.
 let risultato ;
 // input chilometri
@@ -16,10 +13,7 @@ let chilometriUtente = parseInt(prompt("inserisci il numero di chilometri da far
 let etaUtente = parseInt(prompt("inserisci una età (un numero)"));
 
 console.log(chilometriUtente, etaUtente);
-
-
 // ELABPRAZIONE calcolare il prezzo del biglietto in base ai km (0.21 € al km)
- 
 
  // SE l'utente ha >18 <65 anni (adulto)
  if (etaUtente >18 && etaUtente <65) { 
@@ -30,22 +24,19 @@ console.log(chilometriUtente, etaUtente);
     console.log(risultato);
  } 
  // SE l'utente ha <18 anni (minorenne)
- if (etaUtente < 18){
+ else if (etaUtente < 18){
 // ALLORA applicare sconto del 20%
  // ELABORAZIONE moltiplicare il numero dei chilometri per 0.21 e togliere un quinto dal risultato
  let risultato = chilometriUtente * 0.168 ;
 
  console.log(risultato);
- 
 }
-
  // SE l'utente ha >65 anni (anziano)
- if (etaUtente >65) {
+ else  (etaUtente >65) {
  // ALLORA applicare sconto del 40%
  // ELABORAZIONE moltiplicare il numero dei chilometri per 0.21 e togliere due quinti dal risultato
  let risultato = chilometriUtente * 0.126 ;
 
  console.log(risultato);
 }
-
 // OUTPUT- Mostrare prezzo finale , messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
